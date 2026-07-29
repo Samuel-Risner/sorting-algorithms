3efa72dd766e97d18bf3280dadcbbb7e296d8542
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-07-29 15:16:25 +0200

    improved default values
    - option for no waiting
    - sorting ranges starting at 1

--- src/settings.ts
+++ src/settings.ts
@@ -2,16 +2,13 @@ import bubbleSort from "./algorithms/bubble_sort";
 
 export const SETTINGS = {
     DELAY: {
-        OPTIONS_MS: [100, 500],
+        OPTIONS_MS: [0, 100, 500],
         OPTIONS_SEC: [1, 5],
         DEFAULT_MS: 500 // should be one of the above delay values
     },
     INPUT: {
         DEFAULT: [1, 9] as [number, number],
         PRESETS: [
-            [0, 9],
-            [0, 50],
-            [0, 100],
             [1, 9],
             [1, 50],
             [1, 100]
