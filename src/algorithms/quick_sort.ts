255c8df8e1482057073de849d203a4ead61db43b
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-07-29 15:15:00 +0200

    added quicksort

---
+++ src/algorithms/quick_sort.ts
@@ -0,0 +1,72 @@
+import type Sortable from "../dataVisualization/sortable";
+import pause from "../helpers/pause";
+
+function selectPivot(sortable: Sortable, start: number, end: number): number {
+    return end;
+}
+
+async function quickSortRecursive(sortable: Sortable, start: number, end: number, delayMS: number): Promise<void> {
+    if (start >= end)
+        return;
+
+    // select pivot element
+    const pivotIndex = selectPivot(sortable, start, end);
+    const pivotValue = sortable.at(pivotIndex);
+    sortable.select(pivotIndex);
+    await pause(delayMS);
+    
+    // move pivot element to end
+    sortable.select(end);
+    await pause(delayMS);
+    sortable.switch(pivotIndex, end);
+    
+    if (pivotIndex != end)
+        sortable.unselect(pivotIndex);
+    
+    // after this index all elements will be smaller than the pivot element
+    let switchIndex = start;
+    
+    // sort each element (except for the pivot element)
+    for (let i = start; i < end; i++) {
+        sortable.select(i);
+        await pause(delayMS);
+        
+        if (sortable.at(i) < pivotValue) {
+            sortable.select(switchIndex);
+            await pause(delayMS);
+
+            sortable.switch(i, switchIndex);
+            await pause(delayMS);
+
+            sortable.unselect(switchIndex);
+            switchIndex++;
+        }
+
+        sortable.unselect(i);
+    }
+
+    await pause(delayMS);
+
+    sortable.select(switchIndex);
+
+    await pause(delayMS);
+
+    sortable.switch(end, switchIndex);
+
+    await pause(delayMS);
+
+    sortable.unselect(end);
+    sortable.unselect(switchIndex);
+
+    await pause(delayMS);
+
+    await quickSortRecursive(sortable, start, switchIndex-1, delayMS);
+    await pause(delayMS);
+    await quickSortRecursive(sortable, switchIndex+1, end, delayMS);
+}
+
+export default async function quick_sort(sortable: Sortable, delayMS: number): Promise<void> {
+    await quickSortRecursive(sortable, 0, sortable.len()-1, delayMS);
+
+    console.log("Finished quick sort!");
+}
--- src/initHTML/algorithms.ts
+++ src/initHTML/algorithms.ts
@@ -2,6 +2,7 @@ import type Data from "../data";
 import type { T_Algorithm } from "../types";
 import bubbleSort from "../algorithms/bubble_sort";
 import { SETTINGS } from "../settings";
+import quick_sort from "../algorithms/quick_sort";
 
 function createBtn(parent: HTMLElement, text: string, data: Data, algorithm: T_Algorithm): void {
     const btn = document.createElement("button");
@@ -17,7 +18,7 @@ function createBtn(parent: HTMLElement, text: string, data: Data, algorithm: T_A
 }
 
 export default function initAlgorithmHTML(parent: HTMLElement, data: Data): void {
-    // bubble sort
     createBtn(parent, "Bubble Sort", data, bubbleSort);
+    createBtn(parent, "Quick Sort", data, quick_sort);
 
 }
