2f466f74a11c873cec821f96586a26fd3770302e
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-07-30 16:40:40 +0200

    added darkmode

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import Visualizer from "./dataVisualization/visualizer";
 import initDelayHTML from "./initHTML/delay";
 import "./index.css";
 
-const EL_colorModeContainer = document.getElementById("color_mode_container");
+const EL_mainVisual = document.getElementById("main_visual");
+const BTN_step = document.getElementById("btn_step");
 
 const EL_config = document.getElementById("config");
+
+const BTN_lightmode: HTMLButtonElement | null = document.getElementById("btn_lightmode") as HTMLButtonElement | null;
+const BTN_darkmode: HTMLButtonElement | null = document.getElementById("btn_darkmode") as HTMLButtonElement | null;
 
 const BTN_inputDefault: HTMLButtonElement | null = document.getElementById("btn_input_default") as HTMLButtonElement | null;
 const BTN_inputPreset: HTMLButtonElement | null = document.getElementById("btn_input_preset") as HTMLButtonElement | null;
@@ -19,9 +23,6 @@ const EL_inputCustomContainer = document.getElementById("container_input_custom"
 const EL_inputCustomTextArea: HTMLTextAreaElement | null = document.getElementById("textarea_input_custom") as HTMLTextAreaElement | null;
 const BTN_applyCustomInput: HTMLButtonElement | null = document.getElementById("btn_apply_custom_input") as HTMLButtonElement | null;
 
-const EL_mainVisual = document.getElementById("main_visual");
-const BTN_step = document.getElementById("btn_step");
-
 const EL_selectAlgorithm = document.getElementById("select_algorithm");
 
 const EL_delay = document.getElementById("delay");
@@ -30,9 +31,13 @@ const BTN_shuffle = document.getElementById("btn_shuffle");
 const BTN_start = document.getElementById("btn_start");
 
 if (
-    EL_colorModeContainer === null ||
+    EL_mainVisual === null ||
+    BTN_step === null ||
 
     EL_config === null ||
+
+    BTN_lightmode === null ||
+    BTN_darkmode === null ||
 
     BTN_inputDefault === null ||
     BTN_inputPreset === null ||
@@ -44,9 +49,6 @@ if (
     EL_inputCustomTextArea === null ||
     BTN_applyCustomInput === null ||
 
-    EL_mainVisual === null ||
-    BTN_step === null ||
-
     EL_selectAlgorithm === null ||
 
     EL_delay === null ||
@@ -55,6 +57,28 @@ if (
     BTN_start === null
 )
     throw new Error("Could not retrieve element from DOM");
+
+// color scheme
+// (button visibility is done by css)
+
+BTN_lightmode.addEventListener("click", () => {
+    document.documentElement.classList.add("dark");
+    localStorage.colorscheme = "dark";
+});
+
+BTN_darkmode.addEventListener("click", () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.colorscheme = "light";
+});
+
+if (localStorage.colorscheme === "dark") {
+    document.documentElement.classList.add("dark");
+} else if (localStorage.colorscheme === "light") {
+    document.documentElement.classList.remove("dark");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.classList.add("dark");
+}
+
 
 const data = new Data(new Visualizer(EL_mainVisual));
 
