edfb36627d190e28f14282de5633c27a89328830
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-07-30 17:44:49 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/sorting-algorithms/',
-  build: {
-    outDir: './../sorting-algorithms-webapp/'
-  }
-})
